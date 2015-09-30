16cfab8704720e41e88dacd893b4ea2f6ecc740a
Uma Chandran <umachandran6@gmail.com>
2015-09-30 16:50:41 -0400

    Add tests and package.json

--- wikistack/routes/index.js
+++ wikistack/routes/index.js
@@ -69,28 +69,28 @@ var tagStrToArr = function(tagStr) {
 }
 
 router.post('/wiki/:url_name/edit', function(req, res, next) {
-  models.Page.findOne({ url_name: req.params.url_name }, function(err, page) {
+  models.Page.findOne({ url_name: req.params.url_name }, function(err, page) { // find a page with this url_name in the database
     if(!page) return next()
-    var tags = tagStrToArr(req.body.tags)
-    delete req.body.tags
+    var tags = tagStrToArr(req.body.tags) // create an array of tags from the string we recieved in the form
+    delete req.body.tags // WHY? So when we do var key in req.body on line 85, it won't do tags again
 
-    page.tags = [];
+    page.tags = []; // in the page we found, set tags to an empty array
 
-    tags.forEach(function(tag) {
-      page.tags.addToSet(tag)
+    tags.forEach(function(tag) { // go through our array of new tags
+      page.tags.addToSet(tag) // for each of our new tags, add it to the page's tags array. addToSet() ensure's there are no duplicates
     })
 
     //I'm overwriting properites on page that exist in req.body
     //namely title and body
-    for(var key in req.body) {
-      page[key] = req.body[key]
+    for(var key in req.body) { // for each value we recieved in the form 
+      page[key] = req.body[key] // store that value in the key with the same name in page
     }
 
     page.save(function(err, page) {
       console.log(err)
       if(err) return next(err)
       console.log('this is page before redirect', page)
-      res.redirect(page.full_route)
+      res.redirect("page.full_route")
     })
   });
 })
---
+++ wikistack/test/itsalive.js
@@ -0,0 +1,40 @@
+var expect = require('chai').expect
+var chai = require('chai');
+var spies = require('chai-spies');
+chai.use(spies);
+
+it('does simple math', function(){
+	expect(2+2).to.eql(4);
+})
+
+
+it('confirms setTimeout\'s timer accuracy', function (done) {
+    var start = new Date();
+    setTimeout(function () {
+        var duration = new Date() - start;
+        expect(duration).to.be.closeTo(1000, 50);
+        done();
+    }, 1000);
+});	
+
+it('chai spies', function () {
+    
+	var arr = [2, 4, 6];
+	function plusTwo (num){
+	    return num + 2;
+	}
+	
+    plusTwo = chai.spy(plusTwo);
+	arr.forEach(plusTwo);
+	expect(plusTwo).to.have.been.called.exactly(3);
+});	
+
+it('will invoke a function once per element', function () {
+    var arr = ['x','y','z'];
+    function logNth (val, idx) {
+        console.log('Logging elem #'+idx+':', val);
+    }
+    logNth = chai.spy(logNth);
+    arr.forEach(logNth);
+    expect(logNth).to.have.been.called.exactly(arr.length);
+});
